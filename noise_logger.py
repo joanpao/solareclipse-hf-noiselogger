233387ca5501e4c434a433fef4b5c2db9ff43221
EA5JTT <joanpao@gmail.com>
2026-07-25 17:31:27 +0200

    Update noise_logger.py

--- noise_logger.py
+++ noise_logger.py
@@ -1,29 +1,3 @@
-import ctypes
-
-# Parche global para bibliotecas librtlsdr antiguas en macOS / Python 3.14
-_original_getitem = ctypes.CDLL.__getitem__
-
-def _patched_getitem(self, name_or_ordinal):
-    try:
-        return _original_getitem(self, name_or_ordinal)
-    except AttributeError:
-        # Si la función C no existe en el sistema, devuelve una función dummy que no rompe el programa
-        return ctypes.CFUNCTYPE(ctypes.c_int)(lambda *args: 0)
-
-ctypes.CDLL.__getitem__ = _patched_getitem
-
-import csv
-
-
-
-
-
-
-
-
-
-
-
 import csv
 import time
 import numpy as np
