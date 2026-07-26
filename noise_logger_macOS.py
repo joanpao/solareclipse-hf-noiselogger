087db14d23b397b4b839a86e3ffe4a3138df3303
EA5JTT <joanpao@gmail.com>
2026-07-26 10:11:54 +0200

    Add patch header for noise_logger_macOS.pycomment
    Added a patch header for compatibility with older librtlsdr libraries on macOS.

--- noise_logger_macOS.py
+++ noise_logger_macOS.py
@@ -1,7 +1,11 @@
+# ========================================================================
+# Parche noise_logger_macOS.py
+# Parche global para bibliotecas librtlsdr antiguas en macOS / Python 3.14
+# incluir al principio de noise_logger.py 
+# =========================================================================
+
 import ctypes
 
-# Parche global para bibliotecas librtlsdr antiguas en macOS / Python 3.14
-#incluir al principio de noise_logger.py 
 _original_getitem = ctypes.CDLL.__getitem__
 
 def _patched_getitem(self, name_or_ordinal):
@@ -12,3 +16,5 @@ def _patched_getitem(self, name_or_ordinal):
         return ctypes.CFUNCTYPE(ctypes.c_int)(lambda *args: 0)
 
 ctypes.CDLL.__getitem__ = _patched_getitem
+
+# =========================================================================
